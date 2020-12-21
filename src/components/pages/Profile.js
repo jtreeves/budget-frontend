af258984b5e53150c1c18d7ec0bb37720e623318
Jeremy <jjuriz@gmail.com>
2020-12-21 11:32:12 -0500

    [UserNavigation] Created initial user navigation file, added primary links

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -1,7 +1,10 @@
 // Create function
+import UserNavigation from '../elements/UserNavigation'
+
 function Profile(props) {
     return (
         <div>
+            <UserNavigation />
             <h1>Profile</h1>
             <p><strong>Name:</strong> {props.user.name}</p> 
             <p><strong>Email:</strong> {props.user.email}</p> 
