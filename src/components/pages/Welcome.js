d999eae2e46b726546c8dc9be9c0621ae7996eed
delayedaa <delayedaa@gmail.com>
2020-12-21 14:06:21 -0600

    [App.scss] Add styling for Welcome page

--- src/components/pages/Welcome.js
+++ src/components/pages/Welcome.js
@@ -1,13 +1,15 @@
 // Create function
 function Welcome() {
   return (
-    <div className="div-welcome-header">
-      <h1>Welcome</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore dolorum excepturi, pariatur
-        dolore fuga perferendis mollitia deserunt a voluptas assumenda! Consequatur beatae qui
-        dolorem tempora possimus accusantium, fugit eius quidem?
-      </p>
+    <div className="div-welcome-page">
+      <div className="div-welcome-header">
+        <h1>Welcome</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore dolorum excepturi,
+          pariatur dolore fuga perferendis mollitia deserunt a voluptas assumenda! Consequatur
+          beatae qui dolorem tempora possimus accusantium, fugit eius quidem?
+        </p>
+      </div>
     </div>
   );
 }
