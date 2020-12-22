ea875e59fc387672eab21e36d4ab314b164ecbdf
Thomas Gilbert <tcgilbert94@gmail.com>
2020-12-22 12:40:49 -0500

    [all] fixed merge conflicts

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import UserInfo from '../elements/UserInfo'
 import ProfileRoutes from '../elements/ProfileRoutes'
 
-
+// Profile component
 function Profile(props) {
   
   return (
