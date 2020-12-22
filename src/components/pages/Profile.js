a7945d16c9bd9a76dfc4d1dcbd0d3033a2bf97e2
Thomas Gilbert <tcgilbert94@gmail.com>
2020-12-22 12:51:08 -0500

    [profile] added comment

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import UserInfo from '../elements/UserInfo'
 import ProfileRoutes from '../elements/ProfileRoutes'
 
-
+// hi
 function Profile(props) {
   
   return (
