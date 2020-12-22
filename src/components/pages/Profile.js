8ea8d9aad3a61d5b629ecc435380a5bb517de2b7
Thomas Gilbert <tcgilbert94@gmail.com>
2020-12-22 14:30:43 -0500

    troubleshooting

--- src/components/pages/Profile.js
+++ src/components/pages/Profile.js
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import UserInfo from '../elements/UserInfo'
 import ProfileRoutes from '../elements/ProfileRoutes'
 
-
+// asdf
 function Profile(props) {
   
   return (
