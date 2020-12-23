c177e1547e8955d4de7e9428e5c0c644a733c2cf
jtreeves <jackson.t.reeves@gmail.com>
2020-12-23 10:07:09 -0500

    [Profile] Fix divs

--- src/App.js
+++ src/App.js
@@ -71,7 +71,6 @@ function App() {
   return (
     <div className="div-container-app">
       {handleNavBars()}
-      {/* <div className="div-container-pages"> */}
       <Route exact path="/" component={Welcome} />
       <Route path="/about" component={About} />
       <Route path="/signup" component={Signup} />
@@ -92,7 +91,6 @@ function App() {
       <PrivateRoute path="/profile/:ext" component={Profile} user={currentUser} handleLogout={handleLogout} />
 
       <PrivateRoute exact path="/profile" component={Profile} user={currentUser} handleLogout={handleLogout} />
-      {/* </div> */}
     </div>
   );
 
