ce689984d5c5de90101356c6ab128f243a5901e0
Thomas Gilbert <tcgilbert94@gmail.com>
2020-12-30 11:18:16 -0500

    [CategoryCharts] more setup

--- src/components/elements/CategoryChart.js
+++ src/components/elements/CategoryChart.js
@@ -3,39 +3,14 @@ import {
   BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend,
 } from 'recharts';
 
-const data = [
-  {
-    name: 'Page A', pv: 2400
-  },
-  {
-    name: 'Page B', pv: 1398,
-  },
-  {
-    name: 'Page C', pv: 9800,
-  },
-  {
-    name: 'Page D', pv: 3908,
-  },
-  {
-    name: 'Page E', pv: 4800,
-  },
-  {
-    name: 'Page F', pv: 3800,
-  },
-  {
-    name: 'Page G', pv: 4300,
-  },
-];
-
 export default class Example extends PureComponent {
-
-
+  
   render() {
     return (
       <BarChart
         width={500}
         height={300}
-        data={data}
+        data={this.props.data}
         margin={{
           top: 5, right: 30, left: 20, bottom: 5,
         }}
--- src/components/elements/CategoryCharts.js
+++ src/components/elements/CategoryCharts.js
@@ -1,9 +1,33 @@
 import CategoryChart from "./CategoryChart"
 
 function CategoryCharts(props) {
+  const data = [
+    {
+      name: 'Page A', pv: 2400
+    },
+    {
+      name: 'Page B', pv: 1398,
+    },
+    {
+      name: 'Page C', pv: 9800,
+    },
+    {
+      name: 'Page D', pv: 3908,
+    },
+    {
+      name: 'Page E', pv: 4800,
+    },
+    {
+      name: 'Page F', pv: 3800,
+    },
+    {
+      name: 'Page G', pv: 4300,
+    },
+  ];
+
   return (
     <div className="chart-container">
-      <CategoryChart />
+      <CategoryChart data={data}/>
     </div>
   );
 }
