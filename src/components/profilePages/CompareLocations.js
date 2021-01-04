37a4262e8797f3a59efd4d29b1637a1de5c8f1c4
Thomas Gilbert <tcgilbert94@gmail.com>
2021-01-04 12:25:20 -0500

    [CompareLocations] correctly fetching data from numbeo for budget location

--- src/components/profilePages/CompareLocations.js
+++ src/components/profilePages/CompareLocations.js
@@ -1,16 +1,35 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import calcFunctions from "../../utilities/calcFunctions";
 import Cities from "../../utilities/Cities";
+import axios from "axios";
 import AllBudgetsChartTotals from "../elements/AllBudgetsChartTotals";
 
 function CompareLocations(props) {
   const [citiesToCompare, setCitiesToCompare] = useState([]);
   const [selectedCity, setSelectedCity] = useState("Albany, NY");
+  const [budgetLocationCPI, setBudgetLocationCPI] = useState(null);
   const budgetTotals = calcFunctions.calcAllBudgetTotals([props.budget]);
+  const NUMBEO_API_KEY = process.env.NUMBEO_API_KEY;
+
+  useEffect(() => {
+    const fetchCityIndices = () => {
+      fetch(
+        `https://www.numbeo.com/api/indices?api_key=qt1nz2cebg6wjk&query=${props.budget.location}`
+      )
+        .then((res) => {
+          return res.json();
+        })
+        .then((data) => {
+          let cityCpi = data.cpi_and_rent_index
+          console.log(cityCpi);
+          setBudgetLocationCPI(cityCpi)
+        });
+    };
+    fetchCityIndices()
+  }, []);
 
   const addCity = () => {
     let cityAlreadyAdded = false;
-    console.log(selectedCity);
     let arrayCopy = citiesToCompare.slice();
     arrayCopy.forEach((city) => {
       if (city === selectedCity) {
@@ -27,13 +46,12 @@ function CompareLocations(props) {
   };
 
   const deleteCity = (cityObj) => {
-    console.log(cityObj);
     let arrayCopy = citiesToCompare.slice();
     let index = arrayCopy.indexOf(cityObj.city);
     if (index !== -1) {
       arrayCopy.splice(index, 1);
     }
-    setCitiesToCompare(arrayCopy)
+    setCitiesToCompare(arrayCopy);
   };
 
   const cities = citiesToCompare.map((city, idx) => {
@@ -43,6 +61,15 @@ function CompareLocations(props) {
       </button>
     );
   });
+
+  const showCpi = () => {
+    if (budgetLocationCPI) {
+      return <h3>Numbeo Cpi: {budgetLocationCPI}</h3>
+    } else {
+      return <h3>Numbeo Cpi: Loading...</h3>
+    }
+  }
+
   return (
     <div>
       <h2>Compare Locations</h2>
@@ -51,6 +78,7 @@ function CompareLocations(props) {
         <h3>Income: ${budgetTotals[0].totalIncome}</h3>
         <h3>Expenses: ${budgetTotals[0].totalExpense}</h3>
         <h3>Savings: ${budgetTotals[0].totalSavings}</h3>
+        {showCpi()}
         <h3>Add city to compare</h3>
         <Cities
           setLocation={setSelectedCity}
