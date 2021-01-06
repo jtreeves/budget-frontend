c9e266c4f79c0db63c185e7932efdc7e2aac87cc
delayedaa <delayedaa@gmail.com>
2021-01-06 12:00:17 -0600

    Update styling to compare locations chart

--- src/components/elements/CompareLocationsChart.js
+++ src/components/elements/CompareLocationsChart.js
@@ -4,6 +4,31 @@ import {
   BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ComposedChart, ResponsiveContainer
 } from 'recharts';
 
+const CandyBar = (props) => {
+  const {    
+    x: oX,
+    y: oY,
+    width: oWidth,
+    height: oHeight,
+    value,
+    fill
+  } = props;
+  
+  let x = oX;
+  let y = oHeight < 0 ? oY + oHeight : oY;
+  let width = oWidth;
+  let height = Math.abs(oHeight);
+
+  return (
+   <rect fill={fill}
+       mask='url(#mask-stripe)'
+          x={x}
+          y={y}
+          width={width}
+          height={height} />
+    );
+};
+
 const CustomTooltip = ({ payload, active }) => {
   if (active) {
     return (
@@ -28,6 +53,17 @@ export default class CompareLocationsChart extends PureComponent {
           data={this.props.data}
           maxBarSize={100}
         >
+          <pattern id="pattern-stripe" 
+          width="4" height="4" 
+          patternUnits="userSpaceOnUse"
+          patternTransform="rotate(45)">
+          <rect width="2" height="4" transform="translate(0,0)" fill="white">
+          </rect>
+          </pattern>
+          <mask id="mask-stripe">
+          <rect x="0" y="0" width="100%" height="100%" fill="url(#pattern-stripe)" />
+          </mask>
+          
           <XAxis
             dataKey="name"
             tickLine={false}
@@ -38,8 +74,12 @@ export default class CompareLocationsChart extends PureComponent {
             cursor={{ fill: 'rgba(255, 255, 255, 0.5)' }}
           />
           <Legend />
-          <Bar dataKey="Expenses" fill="#eb0000" />
-          <Bar dataKey="Savings" fill="#5cbd3a" />
+          <Bar
+            dataKey="Expenses"
+            shape={<CandyBar />}
+            fill="#aa0100"
+          />
+          <Bar dataKey="Savings" fill="#367724" />
         </BarChart>
       </ResponsiveContainer>
     );
