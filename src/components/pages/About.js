583ca3b56ebef9e7eefe50e361e36c8fdb3f2d20
jtreeves <jackson.t.reeves@gmail.com>
2021-01-06 12:31:17 -0500

    [About] Add intro, screenshots, and kicker

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -4,9 +4,26 @@ function About() {
         <div className="div-public-page">
             <div className="div-public-header">
                 <h1>About</h1>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore dolorum excepturi pariatur dolore fuga perferendis mollitia deserunt a voluptas assumenda! Consequatur beatae qui dolorem tempora possimus accusantium, fugit eius quidem?
-                </p>
+                <p>Kaleidoscope is the app that will revolultionize your financial life.</p>
+                <h2>Main Page</h2>
+                <img src="../../../images/app1.png" />
+                <h2>Log In</h2>
+                <img src="../../../images/app2.png" />
+                <h2>Sign Up</h2>
+                <img src="../../../images/app3.png" />
+                <h2>Create First Budget</h2>
+                <img src="../../../images/app4.png" />
+                <h2>Budget Overview</h2>
+                <img src="../../../images/app5.png" />
+                <h2>Overview for Separate Budget</h2>
+                <img src="../../../images/app6.png" />
+                <h2>Category Breakdown</h2>
+                <img src="../../../images/app7.png" />
+                <h2>Compare Budgets</h2>
+                <img src="../../../images/app8.png" />
+                <h2>Compare Locations</h2>
+                <img src="../../../images/app9.png" />
+                <p>Created by Alan Avery, Thomas Gilbert, Jackson Reeves, and Jeremy Uriz.</p>
             </div>
         </div>
     )
