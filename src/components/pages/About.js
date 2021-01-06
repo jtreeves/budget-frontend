0408b30e999d930b1028bedfc1297b751adfcd84
jtreeves <jackson.t.reeves@gmail.com>
2021-01-06 12:51:20 -0500

    [About] Change remaining image paths from relative to absolute

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -8,21 +8,21 @@ function About() {
                 <h2>Main Page</h2>
                 <img src="https://res.cloudinary.com/alanavery/image/upload/v1609955150/kaleidoscope/app1_tidbt5.png" />
                 <h2>Log In</h2>
-                <img src="../../../images/app2.png" />
+                <img src="https://res.cloudinary.com/alanavery/image/upload/v1609955150/kaleidoscope/app2_seprvx.png" />
                 <h2>Sign Up</h2>
-                <img src="../../../images/app3.png" />
+                <img src="https://res.cloudinary.com/alanavery/image/upload/v1609955150/kaleidoscope/app3_npix3c.png" />
                 <h2>Create First Budget</h2>
-                <img src="../../../images/app4.png" />
+                <img src="https://res.cloudinary.com/alanavery/image/upload/v1609955150/kaleidoscope/app4_hr55ph.png" />
                 <h2>Budget Overview</h2>
-                <img src="../../../images/app5.png" />
+                <img src="https://res.cloudinary.com/alanavery/image/upload/v1609955150/kaleidoscope/app5_jgjjzd.png" />
                 <h2>Overview for Separate Budget</h2>
-                <img src="../../../images/app6.png" />
+                <img src="https://res.cloudinary.com/alanavery/image/upload/v1609955150/kaleidoscope/app6_ivep4j.png" />
                 <h2>Category Breakdown</h2>
-                <img src="../../../images/app7.png" />
+                <img src="https://res.cloudinary.com/alanavery/image/upload/v1609955150/kaleidoscope/app7_tykbhe.png" />
                 <h2>Compare Budgets</h2>
-                <img src="../../../images/app8.png" />
+                <img src="https://res.cloudinary.com/alanavery/image/upload/v1609955150/kaleidoscope/app8_ltu8nd.png" />
                 <h2>Compare Locations</h2>
-                <img src="../../../images/app9.png" />
+                <img src="https://res.cloudinary.com/alanavery/image/upload/v1609955150/kaleidoscope/app9_klysmz.png" />
                 <p>Created by Alan Avery, Thomas Gilbert, Jackson Reeves, and Jeremy Uriz in January 2021. See the code behind this site on the app's <a target="_blank" href="https://github.com/jtreeves/budget-frontend">repository on GitHub</a>.</p>
             </div>
         </div>
