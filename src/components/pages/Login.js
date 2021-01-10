2a689cda5fd221c39d49a3fa5c3fb5a590c9c7a1
jtreeves <jackson.t.reeves@gmail.com>
2021-01-10 11:41:21 -0500

    [Login] Fix tabs

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -3,35 +3,35 @@ import FormGroup from '../elements/FormGroup'
 
 // Create function
 function Login(props) {
-  return (
-    <div id="div-login">
-      <form onSubmit={props.handleLogin}>
-        <FormGroup
-          type="email"
-          label="email"
-          value={props.loginEmail}
-          display="Email"
-          onChange={props.handleLoginEmail}
-        />
-        <FormGroup
-          type="password"
-          label="password"
-          value={props.loginPassword}
-          display="Password"
-          onChange={props.handleLoginPassword}
-        />
-        <button
-          onClick={(e) => {
-            e.preventDefault()
-            props.handleLogin()
-          }}
-          type="submit"
-        >
-          Submit
-        </button>
-      </form>
-    </div>
-  )
+    return (
+        <div id="div-login">
+            <form onSubmit={props.handleLogin}>
+                <FormGroup
+                    type="email"
+                    label="email"
+                    value={props.loginEmail}
+                    display="Email"
+                    onChange={props.handleLoginEmail}
+                />
+                <FormGroup
+                    type="password"
+                    label="password"
+                    value={props.loginPassword}
+                    display="Password"
+                    onChange={props.handleLoginPassword}
+                />
+                <button
+                    onClick={(e) => {
+                        e.preventDefault()
+                        props.handleLogin()
+                    }}
+                    type="submit"
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+    )
 }
 
 // Export function
